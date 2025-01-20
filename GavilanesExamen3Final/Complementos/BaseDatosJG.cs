a87af1f29ef250fcd2c0c8f4c2fbea6d95a664af
JohaoG10 <johao.gavilanes@udla.edu.ec>
2025-01-20 16:54:43 -0500

    Base de datos

--- GavilanesExamen3Final/Complementos/BaseDatosJG.cs
+++ GavilanesExamen3Final/Complementos/BaseDatosJG.cs
@@ -1,12 +1,30 @@
-﻿using System;
+﻿using SQLite;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using System.IO;
 using System.Threading.Tasks;
+using GavilanesExamen3Final.Models;
 
 namespace GavilanesExamen3Final.Complementos
 {
-    internal class BaseDatosJG
+    public class BaseDatosJG
     {
+        private SQLiteAsyncConnection _conexion;
+
+        public BaseDatosJG()
+        {
+            var rutaBaseDeDatos = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "paises.db");
+            _conexion = new SQLiteAsyncConnection(rutaBaseDeDatos);
+            _conexion.CreateTableAsync<PaisJG>().Wait();  
+        }
+
+        public Task<int> GuardarPaisAsync(PaisJG pais)
+        {
+            return _conexion.InsertAsync(pais);
+        }
+
+        public Task<List<PaisJG>> ObtenerPaisesAsync()
+        {
+            return _conexion.Table<PaisJG>().ToListAsync();
+        }
     }
 }
