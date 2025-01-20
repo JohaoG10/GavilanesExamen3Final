44458a0be487bcf7d2cf66c4dc485a34379776dd
JohaoG10 <johao.gavilanes@udla.edu.ec>
2025-01-20 16:40:08 -0500

    CLASE PAISJG

---
+++ GavilanesExamen3Final/Models/PaisJG.cs
@@ -0,0 +1,14 @@
+﻿using SQLite;
+
+namespace AplicacionPais.Models
+{
+    public class PaisJG
+    {
+        [PrimaryKey, AutoIncrement]
+        public int PaisId { get; set; }
+        public string PaisNombre { get; set; }
+        public string ZonaGeografica { get; set; }
+        public string EnlaceMapa { get; set; }
+        public string UsuarioRegistro { get; set; }
+    }
+}
